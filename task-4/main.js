8e6262c6c929d8fbbc01142fd4342e7c6a3727d7
AgalarovBaycan <106188929+AgalarovBaycan@users.noreply.github.com>
2022-06-24 20:51:49 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,12 @@
-const prompt = require('prompt');
-prompt.start();
+const massiv = [ 3, 6, 5, 3 ,5, 5, 4, 5 ,4 , 3 , 2 , 3 , 6 , 4 ]
+let max = 0
+let min = massiv[0]
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    for(i=0;i<massiv.length;i++){
+        if(massiv[i]>max){
+            max = massiv[i]
+        }
+        if(massiv[i]<min){
+            min = massiv [i]
+        }
+    }console.log(max - min); 
